320e5bef5d4a21778aa0e9c2a3bebc6e58c17058
Agatino-P <57068089+Agatino-P@users.noreply.github.com>
2020-09-03 22:42:13 +0200

    Work in progress

---
+++ Mecal.Cam3D.Core/Interfaces/Interface1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mecal.Cam3D.Core.Interfaces
+{
+    interface Interface1
+    {
+    }
+}
---
+++ Mecal.Cam3D.Core/Services/OrderService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mecal.Cam3D.Core.Services
+{
+    class OrderService
+    {
+    }
+}
